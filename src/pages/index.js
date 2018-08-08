1cf8648e716a8130a9e8df7216888417509e6355
bhanusankhyan <bhanusankhyan77@gmail.com>
2018-08-09 02:48:15 +0530

    meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import {Helmet} from 'react-helmet'
 
 const IndexPage = () => (
   <div>
+    <Helmet>
+      <title>Blogsssss</title>
+      <meta name="copyright" content="Copyright by RedCarpet Tech Pvt Ltd. All Rights Reserved." />
+					<meta name="twitter:card" content="summary" />
+					<meta name="twitter:site" content="@RedcarpetUp" />
+					<meta name="twitter:title" content="RedCarpet - Get Instant Credit"/>
+					<meta name="twitter:description" content="RedCarpet helps you buy any product from any site and pay back in installments! With RedCarpet you will:
+✓ Never miss a deal from from any online site, due to lack of funds
+✓ Save your cash for other uses
+✓ Increase the buying power of your pocket money
+✓ Be able to buy an expensive product, without having to worry about paying it upfront." />
+				  <meta name="twitter:image" content="https://media.graphcms.com/CsHErWROTUedcVjgO2kI" />
+
+      </Helmet>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
