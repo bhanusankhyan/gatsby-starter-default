9ecea499f87e7e860ca8b9a19678053378bcc785
bhanusankhyan <bhanusankhyan77@gmail.com>
2018-08-13 15:05:50 +0530

    Dynamic Routing Test_01

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
- // You can delete this file if you're not using it
+exports.onCreatePage = async ({ page, boundActionCreators }) => {
+    const { createPage } = boundActionCreators;
+    if (page.path.match(/^\/blog/)) {
+    page.matchPath = "/blog/:path";
+    createPage(page);
+    }
+    
+  };
---
+++ src/pages/blog.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Redirect} from 'react-router-dom'
+
+export default class testing extends React.Component{
+    render(){
+        return(
+            <div>
+                <div style={{height:"200px"}}/>
+                {
+                    (this.props.location.pathname == '/blog' || this.props.location.pathname == '/blog/')? <div>Blog</div> : ""
+                }
+                {
+                    (this.props.location.pathname == "/blog/now" || this.props.location.pathname == '/blog/now/') ? <div>Now Now</div> :""
+                }
+                {
+                    (this.props.location.pathname != '/blog' || this.props.location.pathname != '/blog/now') ? <Redirect to='/404' /> : ""
+                }
+                
+                </div>
+        )
+    }
+}
