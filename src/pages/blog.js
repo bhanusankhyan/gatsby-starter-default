a515994ec2564210c1984acd4339b705ab1af8ea
bhanusankhyan <bhanusankhyan77@gmail.com>
2018-08-13 15:16:31 +0530

    Route Testing patch v_1.1

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -13,7 +13,7 @@ export default class testing extends React.Component{
                     (this.props.location.pathname == "/blog/now" || this.props.location.pathname == '/blog/now/') ? <div>Now Now</div> :""
                 }
                 {
-                    (this.props.location.pathname != '/blog' || this.props.location.pathname != '/blog/now') ? <Redirect to='/404' /> : ""
+                    (this.props.location.pathname != '/blog' && this.props.location.pathname != '/blog/now') ? <Redirect to='/404' /> : ""
                 }
                 
                 </div>
